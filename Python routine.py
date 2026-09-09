f7c5a9bc4db56c2764abea23a9e6320a7b0fb79a
jayinkishokumar698-ship-it <jayinkishokumar698@gmail.com>
2026-09-09 20:07:07 +0100

    Add files via upload

---
+++ Python routine.py
@@ -0,0 +1,15 @@
+print("My daily routine!")
+
+print("I wake up at 6am and go for a run.")
+
+print ("After my run, I take a shower and have breakfast.")
+
+print("I then go to school and come back.")
+
+print("In the evening, I usually go out with friends and study for 2 hours.")
+
+print("Finally, I eat dinner and go to bed at 10pm and sleep for 8 hours.")
+
+print("Total study hours in a week:",2*7)
+
+print("Total sleep hours in a week:",8*7)
