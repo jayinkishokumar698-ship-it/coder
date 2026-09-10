8d3ce436a3a2a9408aaf22b547ddd7aae7ba7180
jayinkishokumar698-ship-it <jayinkishokumar698@gmail.com>
2026-09-10 20:26:39 +0100

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+print("Welcome to to the world of programming")
+
+print(4)
+
+print("Hello Python \n")
+
+print("Hello" ,5)
+
+print("Welcome to" , end = "*")
---
+++ 3.py
@@ -0,0 +1,11 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to codingal")
