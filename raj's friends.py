0adda9f943d8f2c7cc7ec75fca000b11411a9881
jayinkishokumar698-ship-it <jayinkishokumar698@gmail.com>
2026-09-10 20:51:20 +0100

    Add files via upload

---
+++ raj's friends.py
@@ -0,0 +1,13 @@
+kumar = ("18/12/2012")
+ashwin = ("12/12/2012")
+dhruv = ("04/06/2013")
+prakash = ("09/10/2012")
+sam = ("23/03/2013")
+
+print("My friends birthday are as follows")
+
+print(kumar)
+print(ashwin)
+print(dhruv)
+print(prakash)
+print(sam)
